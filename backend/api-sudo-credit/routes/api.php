65f454f4bb520560aa12194dc49a632150538b68
Boris Raouph <raouphboris19@gmail.com>
2025-09-11 15:51:06 +0100

    Commit api4

--- backend/api-sudo-credit/routes/api.php
+++ backend/api-sudo-credit/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user("api3");
+    return $request->user("api4");
 });
 
 
