9c2a96dea9fc64a6aee36a391471eae78ac5d6c7
Boris Raouph <raouphboris19@gmail.com>
2025-09-18 10:23:54 +0100

    Commit pour backend des roles

--- backend/api-sudo-credit/database/seeders/DatabaseSeeder.php
+++ backend/api-sudo-credit/database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        // Appeler ton seeder ici
+        $this->call(PermissionsDemoSeeder::class);
     }
 }
--- backend/api-sudo-credit/routes/api.php
+++ backend/api-sudo-credit/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RolesPermissionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,4 +34,15 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
+
+Route::group([
+ 
+    // 'middleware' => 'auth:api',
+    'prefix' => 'auth',
+    // 'middleware' => ['auth:api'],
+ 
+], function ($router) {
+    Route::resource('roles', RolesPermissionController::class);;
+   
 });
